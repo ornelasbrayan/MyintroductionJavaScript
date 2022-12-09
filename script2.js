51fdfb13ce75c445b55395a5a4967c457e6a0e9d
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2022-12-08 20:50:05 -0700

    Calculating Taxes exercise

--- script2.js
+++ script2.js
@@ -57,8 +57,16 @@ let total=0;
 
 function addCart(price){
     total=total+price;
-    console.log(total)
+    console.log(total);
     return total;
 
+}
+
+function calculateTaxes(){
+    subtotal=addCart(2);
+    let totalTax= subtotal*1.11;
+    document.getElementById("receipt").innerHTML=
+    `<p>Subtotal= ${subtotal}</p>
+    <p>Total= ${totalTax}</p>`;
 }
 
