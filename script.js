e2bb094e9e62902085d075dc0ea63cad07e50ccb
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2022-12-08 23:10:31 -0700

    Class 103 Assignment 1

--- script.js
+++ script.js
@@ -31,10 +31,37 @@ let jobTitle="Mechatronic Engineering"
 // document.write("You will be a "+jobTitle+ " in "+geoLocation+", and married to "+partnerName+" with " +numChildren+" kids");
 
 //template string
+// document.write(`
+// <p>You will be a ${jobTitle} </p>
+// <p>in ${geoLocation}, and married to ${partnerName} </p> 
+// <p>with ${numChildren} kids.</p>`)
+
+let product="Filament PLA";
+let available=43;
+let price=439;
+let freeShipping="Tuesday 14, December";
+let adress="Tijuana";
+let zipCode="22204";
+let shipFrom="Amazon";
+let seller="Xington Zhilian";
+let stars=4;
+let ratings=3881;
+let policy="It can be returned until January 31, 2023";
+let productWeight="1 kg";
+let brand="XINGTONGZHILIAN";
+let material="Polylactic Acid";
+let color="White";
+let packageDimension="20.4 x 19.4 x 6.6 cm";
+
 document.write(`
-<p>You will be a ${jobTitle} </p>
-<p>in ${geoLocation}, and married to ${partnerName} </p> 
-<p>with ${numChildren} kids.</p>`)
+<p> Name of product: ${product} Available: ${available} Price: $${price}</p>
+<p> Free Delivery on: ${freeShipping} Adress: ${adress} Zip Code: ${zipCode}</p>
+<p> Ship From: ${shipFrom} Seller: ${seller} Stars: ${4} Ratings: ${ratings}</p>
+<p> Return policy: ${policy} Product Weight: ${productWeight} Brand: ${brand}</p>
+<p> Material: ${material} Color: ${color} Package Dimensions: ${packageDimension}</p>`
+
+)
+
 
 
 
