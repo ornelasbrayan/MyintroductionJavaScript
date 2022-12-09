60d6930755bf0c9de6c8705e79f752d9a12574aa
ornelasbrayan <ornelas.brayan@uabc.edu.mx>
2022-12-08 19:38:10 -0700

    function exercise

--- script2.js
+++ script2.js
@@ -1,13 +1,38 @@
-const userName= "Brayan";
-let email="ornelas.brayan@uabc.edu.mx";
-let password="Brayan123";
-let age=20;
-let country="México";
-let salary=100;//monthly salary 
+// let userName;
+// let userEmail;
+// let password;
+// let age;
+// let country;
+// let salary;//monthly salary 
 
-document.write(`<p>Name: ${userName} </p>
-<p> Email: ${email} </p>
-<p> password ${password} </p>
-<p> Age: ${age} </p>
-<p> Country: ${country} </p> 
-<p> Annual Salary: ${salary*12}</p>`);
+// userName = prompt("Enter your User Name");
+// userEmail = prompt("Enter your Email");
+// password = prompt("Enter your password");
+// age = prompt("Enter your Age");
+// country = prompt("Enter your Country");
+// salary = prompt("Enter your monthly salary");
+
+// document.write(`<p>Name: ${userName} </p>
+// <p> Email: ${userEmail} </p>
+// <p> password ${password} </p>
+// <p> Age: ${age} </p>
+// <p> Country: ${country} </p> 
+// <p> Annual Salary: ${salary*12}</p>`);
+
+// creating function 
+
+// function declaration 
+function greeting(){
+    console.log("Hello world");
+    // alert("Hello world");
+    // document.write("Hello world");
+    return "Brayan";
+}
+
+function sum(){
+    return 10+10;
+}
+
+console.log(sum());
+
+// function expression 
